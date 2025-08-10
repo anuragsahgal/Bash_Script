a261286c93bba0f547b062231ee5ea927538d29e
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-11 05:09:18 +0530

    User Interation

---
+++ User Interaction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 11/08/2025
+###############
+
+#User Interaction
+
+echo "What is your name?"
+read name
+
+echo "Your name is $name."
+
+echo "And what is your age?"
+
+read age
+echo "Your age is $age."
