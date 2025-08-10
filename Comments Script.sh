6575cf7efcc54302b09e74c037c235cfa813a129
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-11 03:50:51 +0530

    Use of Comments

---
+++ Comments Script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 11/08/2025
+###############
+
+#How to use Single line comments & Multi line comments
+
+#This is a single line comment
+Echo "Checking Comment"
+
+<<comment
+This
+is 
+multi
+line comments
+
+comment
+#Use everytime comment in last line for closing multi comment
