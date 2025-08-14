989dc88f3f4ddf5b8c957a6e6cb717c94f38a680
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 03:29:31 +0530

    Arguments

---
+++ Arguments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 15/08/2025
+###############
+
+#to access the arguments
+
+echo "First argument is $1"
+echo "Seconf argument is $2"
+
+echo "all the arguments are - $@"
+echo "number off arguments are - $#"
+
+#for loop to access the values from arguments
+for filename in $@
+do
+echo "copying file - $filename"
+done
