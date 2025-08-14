6001d080d0cf82a4c319304c17bc81ac1276af93
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 01:57:12 +0530

    While Loop

---
+++ Infinte Loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 15/08/2025
+###############
+
+#Infinte Loop
+
+while true 
+do
+echo "Hi Anurag"
+sleep 4s
+done
---
+++ While Loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 12/08/2025
+###############
+
+count=0
+num=10
+
+while [[ $count -le $num ]]
+do
+echo "Value of count var is $count"
+let count++
+done
