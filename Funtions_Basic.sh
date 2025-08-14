49f698be4eb6a5adb059cd060fd87843d6c52dda
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 02:43:21 +0530

    Functions_Basic

---
+++ Funtions_Basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 15/08/2025
+###############
+
+function welcomenote {
+    echo "Hi Anurag"
+}
+
+#to call our function
+welcomenote
+welcomenote
+welcomenote
