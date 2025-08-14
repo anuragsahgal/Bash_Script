590f05b83448f2e689c406cdbead6a9182e992cc
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 03:21:27 +0530

    Function with arguments

---
+++ Function_With_arguments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 15/08/2025
+###############
+
+function welcomenote {
+    echo "Welcome $1"
+    echo "age is $2"
+}
+
+#to call our function
+welcomenote Anurag 25
+welcomenote RamLakhan 20
+welcomenote Mukesh 20
