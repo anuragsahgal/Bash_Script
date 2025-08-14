53e5dec3ed220cb379c3e3ae777174fb629b4820
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 03:33:17 +0530

    Shift_Arguments

---
+++ Shift_Arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 15/08/2025
+###############
+
+#to create a user, provide username and description
+echo "Creating user"
+echo "Username is $1"
+
+shift 
+echo "Description is $@"
