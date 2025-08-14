5cecd3eb0eb2adc39754ecc65a00c3c34bcd4313
Anurag Sahgal <anuragsahgal2@gmail.com>
2025-08-15 00:58:23 +0530

    For Loop Basic

---
+++ for_loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###############
+# Author = Anurag Sahgal
+# Crating Date = 12/08/2025
+###############
+
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+echo "Number is $i"
+done
+
+for name in Anurag Mukesh Ram
+do
+echo "Name is $name"
+done
+
+for o in {1..20} 
+do
+echo "Number is $o"
+done
